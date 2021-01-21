72f721ba4269cf63424e7d35bb0afdb384fea681
이동욱 <jojoldu@gmail.com>
2021-01-21 09:21:45 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/blogcode/springbatch/integrationin/SpringBatchIntegrationInActionApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.springbatch.integrationin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchIntegrationInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchIntegrationInActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jojoldu/blogcode/springbatch/integrationin/SpringBatchIntegrationInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.springbatch.integrationin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchIntegrationInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
